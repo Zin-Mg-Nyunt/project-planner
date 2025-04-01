bdf3a764c36e39a7c74fcc6e26386058a3b8f9d5
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-02 01:56:44 +0800

    add filter nav feature and data pasing between parent and child

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,36 @@
+<template>
+      <nav>
+      <button 
+        class="p-3 uppercase text-sm tracking-wide cursor-pointer font-bold text-gray-400" 
+        @click="filterBy('all')"
+        :class="{active:current==='all'}"
+      >all</button>
+      <button 
+        class="p-3 uppercase text-sm tracking-wide cursor-pointer font-bold text-gray-400"
+        @click="filterBy('ongoing')"
+        :class="{active:current==='ongoing'}"
+      >ongoing</button>
+      <button 
+        class="p-3 uppercase text-sm tracking-wide cursor-pointer font-bold text-gray-400" 
+        @click="filterBy('complete')"
+        :class="{active:current==='complete'}"
+      >complete</button>
+    </nav>
+</template>
+
+<script>
+export default {
+    props:['current'], // receive data from parent
+    methods:{
+        filterBy(value){
+            this.$emit('filterBy',value) // send data to parent because need to filter projects in parent
+        }
+    }
+}
+</script>
+
+<style>
+    .active{
+        color: black;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
+    <FilterNav @filterBy="current=$event" :current="current"></FilterNav>
+
     <div class="h-[500px] overflow-auto">
       <!-- check the data and give feedback -->
       <div v-if="projects.length==0">
         <h1 class="text-center font-[900] uppercase tracking-wide text-3xl text-gray-400">Create Project</h1>
       </div>
+
       <div v-for="project in projects" :key="project.id" class="bg-gray-200 my-2 px-5 py-10 rounded-lg border-l-6 border-red-400 " :class="{green:project.complete}">
         <SingleProject :project="project" @updateComplete="updateComplete" @deleteProject="deleteProject"></SingleProject>
       </div>
@@ -12,16 +15,19 @@
 
 <script>
 
+import FilterNav from '../components/FilterNav'
 import SingleProject from '../components/SingleProject.vue'
 export default {
-  data(){
-    return {
-      projects :[]
-    }
-  },
   components: {
+    FilterNav,
     SingleProject,
     
+  },
+  data(){
+    return {
+      projects :[],
+      current:'all' // to filter projects
+    }
   },
   mounted(){
     fetch('http://localhost:3000/projects')
