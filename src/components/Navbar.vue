4161c1538ce80fb6dd2313fe5abe127a4d0fffef
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-12-06 19:31:35 +0700

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link
+    :to="{ name: 'projects' }"
+    class="relative before:content-[''] before:absolute before:left-0 before:-bottom-[7px] before:h-[2px] before:w-0 before:bg-green-400 transition-all before:duration-500"
+    >Projects</router-link
+  >
+  <router-link
+    :to="{ name: 'add-project' }"
+    class="relative before:content-[''] before:absolute before:left-0 before:-bottom-[7px] before:h-[2px] before:w-0 before:bg-green-400 transition-all before:duration-500"
+    >Add Project</router-link
+  >
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.router-link-active::before {
+  width: 100%;
+}
+</style>
